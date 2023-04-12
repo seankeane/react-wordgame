ecac6c397626956b316338779578d47dfaf420b5
Sean Keane <seankeane@live.ie>
2023-04-13 00:15:54 +0100

    App.test.js running and checking for answerlength

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react';
+import React, { useState as useStateMock } from 'react'
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}))
+
+describe('Header', () => {
+  const setState = jest.fn();
+
+  beforeEach(() => {
+    useStateMock.mockImplementation(init => [init, setState])
+  });
+
+  test('renders answer length pickers', () => {
+    render(<App />);
+    const button = screen.getByText(/CLICK HERE TO CONFIRM ANSWER LENGTH/i);
+    const plus = screen.getByText("+");
+    const minus = screen.getByText("-");
+    expect(button).toBeInTheDocument();
+    expect(plus).toBeInTheDocument();
+    expect(minus).toBeInTheDocument();
+  });
+
+  test('choose answer length 6', () => {
+    render(<App />);
+    const plus = screen.getByText("+");
+    fireEvent.click(plus);
+    const button = screen.getByText(/CLICK HERE TO CONFIRM ANSWER LENGTH/i);
+    fireEvent.click(button);
+    expect(setState).toHaveBeenCalledWith(6);
+  });
+
+  test('choose answer length 4', () => {
+    render(<App />);
+    const minus = screen.getByText("-");
+    fireEvent.click(minus);
+    const button = screen.getByText(/CLICK HERE TO CONFIRM ANSWER LENGTH/i);
+    fireEvent.click(button);
+    expect(setState).toHaveBeenCalledWith(4);
+  });
+
 });
